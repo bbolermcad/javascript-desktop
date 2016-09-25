e5fb81c10056c45e7594981907ace0e5303cc320
Brigit Boler <bboler@mcad.edu>
2016-09-25 17:15:01 -0500

    setting up variables

--- week4/assign4/script.js
+++ week4/assign4/script.js
@@ -1,9 +1,30 @@
+var wchurchill = {
+  name: "Winston Churchill",
+  date: "June 18, 1940",
+  BCE: "false",
+}
+
+var mghandi = {
+  name: "Mahatma Gandhi",
+  date: "August 8, 1942",
+  BCE: "false",
+}
+
+var demonthenes = {
+  name: "Demosthenes",
+  date: "342 B.C.E.",
+  BCE: "true",
+}
+
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
+
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Churchill" button.
+
+
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
