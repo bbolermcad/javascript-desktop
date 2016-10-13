dc7a6a5a0cb7701e44b03e342f6c0a0c1eee1db9
Brigit Boler <bboler@mcad.edu>
2016-10-12 21:37:00 -0500

    i tried with the help of others

--- week6/assign6/oldcode.js
+++ week6/assign6/oldcode.js
@@ -15,3 +15,36 @@ switch(favoriteSpeechPrompt){
     console.log('Did you spell that name correctly?');
     break;
 }
+
+/*switch(favoriteSpeechPrompt){
+  case 'Churchill':
+    console.log(speechesArray[0].author + ' was ' + speechesArray[0].authorAge + ' during this speech.');
+    break;
+  case 'Ghandi':
+    console.log(speechesArray[1].author + ' was ' + speechesArray[1].authorAge + ' during this speech.');
+    break;
+  case 'Demosthenes':
+    console.log(speechesArray[2].author + ' was ' + speechesArray[2].authorAge + ' during this speech.');
+    break;
+  default:
+    console.log('Did you spell that name correctly?');
+    break;
+}*/
+
+/*if(speechesArray[0].year < speechesArray[1].year && speechesArray[0].year < speechesArray[2].year){
+  console.log('This is the oldest speech on the page.');
+}else if(speechesArray[0].year > speechesArray[1].year && speechesArray[0].year > speechesArray[2].year){
+  console.log('This is the most recent speech on the page.');
+}*/
+
+/*if(speechesArray[1].year < speechesArray[0].year && speechesArray[1].year < speechesArray[2].year){
+  console.log('This is the oldest speech on the page.');
+}else if(speechesArray[1].year > speechesArray[0].year && speechesArray[1].year > speechesArray[2].year){
+  console.log('This is the most recent speech on the page.');
+}*/
+
+/*if(speechesArray[2].year < speechesArray[0].year && speechesArray[2].year < speechesArray[1].year){
+  console.log('This is the oldest speech on the page.');
+}else if(speechesArray[2].year > speechesArray[0].year && speechesArray[2].year > speechesArray[1].year){
+  console.log('This is the most recent speech on the page.');
+}*/
--- week6/assign6/script.js
+++ week6/assign6/script.js
@@ -28,22 +28,22 @@ var churchillSpeech = {
        i ++;
      }
 
-var recentyear = speechesArray[0].year;
-var oldestyear = speechesArray[0].year;
+     var recentyear = speechesArray[0].year;
+     var oldestyear = speechesArray[0].year;
 
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
   favoriteSpeechPrompt = window.prompt('Which speech author is your favorite?');
 
-  for (var i = 0; i < speechesArray.length; i++) {
+  for (i = 0; i < speechesArray.length; i++) {
   if (favoriteSpeechPrompt === speechesArray[i].author)
   {
     console.log(speechesArray[i].author + " was " + speechesArray[i].authorAge + " during this speech.")
   }
-  else {
+  /*else {
     console.log("Did you spell that right?")
-  }
+  }*/
 }
 
 
@@ -62,13 +62,27 @@ document.getElementById('BtnChurchill').addEventListener('click', function(){
   }
 
 
-    for (var i = 0; i < speechesArray.length; i++) {
+    /* this was my Code
+    for (i = 0; i < speechesArray.length; i++) {
       if (speechesArray[i].year > oldestyear) {
         console.log("This is the newest speech on the page")
       }
 
-      else if (speechesArray[i].year < oldestyear) {
+      if (speechesArray[i].year < oldestyear) {
         console.log("This is the oldest speech on the page.")
+      }
+    }
+
+    This is the code i learned from Kellen Anders because it made the most sense to me when I read it but it actually still does not work for me */
+
+    for (i = 0; i < speechesArray.length; i++) {
+      while (speechesArray[i].year < oldestyear) {
+        oldestyear = speechesArray[i].year;
+        console.log("This is the oldest speech on the page.");
+      };
+      while (speechesArray[i].year > recentyear) {
+        recentyear = speechesArray[i].year;
+        console.log("THis is the most recent speech on the page");
       }
     }
 
