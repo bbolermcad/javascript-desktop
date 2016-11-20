e49998cfbc02a779abb1ac5cbf178851f0584f8c
Brigit Boler <bboler@mcad.edu>
2016-11-20 15:48:51 -0600

    trying to get the positioning to work

---
+++ week12/script.js
@@ -0,0 +1,53 @@
+$("document").ready(function() {
+	$(".recipe").hide();
+	$("h1").animate({
+    width: "100%",
+    opacity: 1,
+    fontSize: "3em",
+  }, 1500 );
+
+	//one user interaction
+	$(".r1").click(function(){
+		$(".recipe").hide()
+		$("#recipeone").show();
+		$("q:first").animate({"right": "+=50px"}, "slow");
+	});
+
+	//one fade event
+	$(".r2").click(function(){
+		$(".recipe").hide()
+		$("#recipetwo").fadeTo("slow", 1);
+	});
+
+	$(".r3").click(function(){
+		$(".recipe").hide()
+		$("#recipethree").show();
+	});
+
+	$(".r4").click(function(){
+		$(".recipe").hide()
+		$("#recipefour").show();
+	});
+
+	$(".r5").click(function(){
+		$(".recipe").hide()
+		$("#recipefive").show();
+	});
+
+	$(".r6").click(function(){
+		$(".recipe").hide()
+		$("#recipesix").show();
+	});
+
+	//second user interaction
+	$(".r7").mouseenter(function(){
+		$(".recipe").hide()
+		$("#recipeseven").show();
+	});
+
+		$(".r7").mouseleave(function(){
+		$(".recipe").hide()
+		$("#recipeseven").hide();
+	});
+
+});
