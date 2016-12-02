97cd73d98c8ce8cbe869f7a40979521b81b2374d
Brigit Boler <bboler@mcad.edu>
2016-12-01 22:49:29 -0600

    ending

--- week13/assign13/script.js
+++ week13/assign13/script.js
@@ -7,10 +7,75 @@ $( document ).ready(function() {
 	})
 
 	$("#alabama").click(function() {
-		var answer=prompt("What is the capital of Alabama?")
+		var answer=prompt("What is the capital of Alabama?");
 
 		if (answer="Montgomery") {
 			$("#alabama").hide('slow');
 		}
+
+		else {
+			prompt("Try again!");
+		}
+	});
+
+	$("#alaska").click(function() {
+		var answer=prompt("What is the capital of Alaska?");
+
+		if (answer="Juneau") {
+			$("#alaska").hide('slow');
+		}
+
+		else {
+			prompt("Try again!");
+		}
+	});
+
+	$("#arizona").click(function() {
+		var answer=prompt("What is the capital of Arizona?");
+
+		if (answer="Phoenix") {
+			$("#arizona").hide('slow');
+		}
+
+		else {
+			prompt("Try again!");
+		}
+	});
+
+	$("#arkansas").click(function() {
+		var answer=prompt("What is the capital of Arkansas?");
+
+		if (answer="Little Rock") {
+			$("#arkansas").hide('slow');
+		}
+
+		else {
+			prompt("Try again!");
+		}
+	});
+
+	$("#california").click(function() {
+		var answer=prompt("What is the capital of California?");
+
+		if (answer="Sacramento") {
+			$("#california").hide('slow');
+		}
+
+		else {
+			prompt("Try again!");
+		}
+	});
+
+	$("#colorado").click(function() {
+		var answer=prompt("What is the capital of Colorado?");
+
+		if (answer="Denver") {
+			$("#colorado").hide('slow');
+		}
+
+		else {
+			prompt("Try again!");
+		}
 	});
 });
+
